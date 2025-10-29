d42340e55ea1c14bc621d1877a9a31e9d72a0297
sikhakumari987 <sikhachandra541@gmail.com>
2025-10-29 21:40:50 +0530

    Otp change to OTP

--- model/Otp.js
+++ model/Otp.js
@@ -4,8 +4,8 @@ const OTPSchema = new mongoose.Schema({
   email: { type: String, required: true },
   code: { type: String, required: true },
   expires: { type: Date, required: true },
-  password: { type: String },  // ✅ temp store password
-  name: { type: String }       // ✅ temp store name
+  password: { type: String },  //temp store password
+  name: { type: String }       //temp store name
 });
 
 module.exports = mongoose.model("OTP", OTPSchema);
