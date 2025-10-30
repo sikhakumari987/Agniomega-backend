a44829b2a581b579635e7390859cc8d23d1af0df
sikhakumari987 <sikhachandra541@gmail.com>
2025-10-30 14:27:20 +0530

    Final rename to OTP.js with correct casing

---
+++ model/OTP.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const OTPSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  code: { type: String, required: true },
+  expires: { type: Date, required: true },
+  password: { type: String },  //temp store password
+  name: { type: String }       //temp store name
+});
+
+module.exports = mongoose.model("OTP", OTPSchema);
